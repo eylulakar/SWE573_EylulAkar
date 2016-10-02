0434e272a67e522a040913f9adbc88feb3f29ea2
EylülAkar <eylul.akar88@gmail.com>
2016-10-02 22:19:29 +0300

    Initialization of the project.
    Initialization of the project.

---
+++ StayHealthy/src/com/javaproject/com/IndexServlet.java
@@ -0,0 +1,42 @@
+package com.javaproject.com;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class IndexServlet
+ */
+@WebServlet("/index")
+public class IndexServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public IndexServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		PrintWriter out = response.getWriter();
+	  
+		out.println("<h1>Stay Healthy</h1>");		
+		out.println("<h2>This is a health tracker application.</h2>");
+		out.println("<p>The main goal of this application is to keep user motivated for eating healthily and maintain a healthy living style.");
+		out.println("</p>");
+	}
+
+	 
+}

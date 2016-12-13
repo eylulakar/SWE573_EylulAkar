27ef584791b2fbc54fed77ddd60834e173291244
eylulakar <eylul.akar88@gmail.com>
2016-12-13 08:06:10 +0300

    Updated user service.

--- StayHealthyApplication/src/main/java/service/UserService.java
+++ StayHealthyApplication/src/main/java/service/UserService.java
@@ -3,6 +3,7 @@ package service;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import business.GeneralFunctions;
 import data.DatabaseFunctions;
 
 public class UserService {
@@ -16,32 +17,63 @@ public class UserService {
 			while (rs.next()) {
 				userEntity = new entity.User();
 				userEntity.Id = Integer.parseInt(rs.getString("Id"));
-				userEntity.Email = rs.getString("Email");		
-				userEntity.FullName = rs.getString("FullName");	
+				userEntity.Email = rs.getString("Email");
+				userEntity.FullName = rs.getString("FullName");
 			}
 
 		} catch (ClassNotFoundException e) {
-			 
+
 		} catch (SQLException e) {
-			
+
 		}
-		
+
 		dto.UserDto userDto = null;
-		
-		if(userEntity!=null)
-		{
+
+		if (userEntity != null) {
 			userDto = new dto.UserDto();
 			userDto.Email = userEntity.Email;
 			userDto.FullName = userEntity.FullName;
 			userDto.Id = userEntity.Id;
-		}		
+		}
 
 		return userDto;
 	}
-	
+
+	public static dto.UserDto GetUserDtoById(int UserId) {
+		dto.UserDto userDto = new dto.UserDto();
+
+		try {
+			ResultSet rs = DatabaseFunctions.GetUserById(UserId);
+
+			while (rs.next()) {
+				userDto.Id = Integer.parseInt(rs.getString("Id"));
+				userDto.Email = rs.getString("Email");
+				userDto.FullName = rs.getString("FullName");
+				userDto.Gender = rs.getInt("Gender");
+				userDto.DateOfBirth = rs.getDate("DateOfBirth");
+				userDto.Weight = rs.getInt("Weight");
+				userDto.Height = rs.getInt("Height");
+				userDto.Bmi = rs.getDouble("Bmi");
+				userDto.Notes = rs.getString("Notes");
+				userDto.DateOfBirthText = rs.getDate("DateOfBirth").toString(); //.replace(" 00:00:00.0","");
+			}
+
+		} catch (ClassNotFoundException e) {
+
+		} catch (SQLException e) {
+
+		}
+
+		return userDto;
+	}
+
 	public static Integer InsertUser(dto.UserDto userDto) {
 
 		Integer result;
+
+		// Calculate bmi value before inserting user to database.
+		double bmi = userDto.Weight / ((userDto.Height / 100) * (userDto.Height / 100));
+		bmi = GeneralFunctions.RoundDouble(bmi, 2);
 
 		try {
 			result = DatabaseFunctions.InsertUser(userDto);
@@ -54,5 +86,26 @@ public class UserService {
 		return result;
 
 	}
-	
+
+	public static Integer UpdateUser(dto.UserDto userDto) {
+
+		Integer result;
+
+		// Calculate bmi value before updating user entity.
+		double bmi = (double)userDto.Weight / (((double)userDto.Height / 100) * ((double)userDto.Height / 100));
+		bmi = GeneralFunctions.RoundDouble(bmi, 2);
+
+		userDto.Bmi = bmi;
+		
+		try {
+			result = DatabaseFunctions.UpdateUser(userDto);
+		} catch (ClassNotFoundException e) {
+			result = -1;
+		} catch (SQLException e) {
+			result = -1;
+		}
+
+		return result;
+
+	}
 }

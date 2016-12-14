e53673f780ea45975b5ac4da17c105f7e73b8f5e
eylulakar <eylul.akar88@gmail.com>
2016-12-15 00:35:17 +0300

    Food & physical acitivty listind and deletion funcitons updated.

--- StayHealthyApplication/src/main/java/controller/UserController.java
+++ StayHealthyApplication/src/main/java/controller/UserController.java
@@ -20,6 +20,7 @@ import com.google.gson.JsonObject;
 
 import model.Person;
 import model.UserModel;
+import service.UserPhysicalActivityService;
 import service.UserService;
 
 @Controller
@@ -89,6 +90,8 @@ public class UserController {
 		userDto.Gender = Integer.parseInt(request.getParameter("formRegisterGender"));
 		userDto.Email = request.getParameter("formRegisterEmail");
 		userDto.Password = request.getParameter("formRegisterPassword");
+		userDto.Weight = Integer.parseInt(request.getParameter("formRegisterWeight"));
+		userDto.Height = Integer.parseInt(request.getParameter("formRegisterHeight"));
 
 		SimpleDateFormat format = new SimpleDateFormat("dd-MM-yyyy");
 
@@ -151,6 +154,95 @@ public class UserController {
 		return responseText;
 	}
 
+	@RequestMapping(value = "/GetUserProfileInfo", method = RequestMethod.GET)
+	public @ResponseBody String GetUserProfileInfo(HttpServletRequest request, HttpServletResponse response) {
+
+		boolean isSuccess = true;
+		String returnMessage = "";
+
+		int userId = GetCurrentUserId(request);
+
+		dto.UserDto userDto = UserService.GetUserDtoById(userId);
+		double bmi = userDto.Bmi;
+
+		String bmiHtml = "";
+
+		if (bmi < 18.5) {
+			bmiHtml = "<div class=\"alert alert-black\"><h4>BMI: " + String.valueOf(bmi) + "</h4> <span>Under ideal range, you might wanna start gaining weight.</span> </div>";
+		} else if (bmi >= 18.5 && bmi <23) {
+			bmiHtml = "<div class=\"alert alert-success\"><h4>BMI: " + String.valueOf(bmi) + "</h4> <span>Healthy range, keep doing what you're doing!</span> </div>";
+		} else if (bmi >= 23 && bmi <= 27.5) {
+			bmiHtml = "<div class=\"alert alert-warning\"><h4>BMI: " + String.valueOf(bmi) + "</h4> <span>Above normal, you might wanna start losing weight!</span> </div>";
+		} else if (bmi > 27.5) {
+			bmiHtml = "<div class=\"alert alert-danger\"><h4>BMI: " + String.valueOf(bmi) + "</h4> <span>Very high, you might wanna get professional help and start losing weight.</span> </div>";
+		}
+		
+		Gson gson = new Gson();
+		JsonElement jsonReturnMessage = gson.toJsonTree(returnMessage);
+
+		JsonObject myObj = new JsonObject();
+
+		myObj.addProperty("success", isSuccess);
+		myObj.add("message", jsonReturnMessage);
+		myObj.add("userDto", gson.toJsonTree(userDto));
+		myObj.add("bmiHtml", gson.toJsonTree(bmiHtml));
+		
+		return myObj.toString();
+	}
+	
+	
+	@RequestMapping(value = "/UpdateUserInformations", method = RequestMethod.GET, consumes = "application/json")
+	public @ResponseBody String UpdateUserInformations(HttpServletRequest request, HttpServletResponse response) {
+
+		boolean isSuccess = true;
+		String returnMessage = "";
+
+		int userId = GetCurrentUserId(request);
+
+		SimpleDateFormat format = new SimpleDateFormat("dd-MM-yyyy");
+
+		java.util.Date dateOfBirth = new java.util.Date();
+		try {
+			dateOfBirth = format.parse(request.getParameter("inputDateOfBirth"));
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+
+		java.sql.Date dateOfBirthFinal = new java.sql.Date(dateOfBirth.getTime());
+
+		dto.UserDto userDto = new dto.UserDto();
+	
+		userDto.Id = userId;
+		userDto.Weight = Integer.parseInt(request.getParameter("inputWeight"));
+		userDto.Height = Integer.parseInt(request.getParameter("inputHeight"));
+		userDto.Gender = Integer.parseInt(request.getParameter("selectGender"));
+		userDto.Notes = request.getParameter("inputNotes");
+		userDto.DateOfBirth = dateOfBirthFinal;
+	
+		int resultId = UserService.UpdateUser(userDto);
+
+		if (resultId > 0) {
+			returnMessage = "Your informations were updated successfully.";
+			isSuccess = true;
+		} else {
+			returnMessage = "Error occured during process. Please try again later.";
+			isSuccess = false;
+		}
+
+		Gson gson = new Gson();
+		JsonElement jsonReturnMessage = gson.toJsonTree(returnMessage);
+
+		JsonObject myObj = new JsonObject();
+
+		myObj.addProperty("success", isSuccess);
+		myObj.add("message", jsonReturnMessage);
+
+		return myObj.toString();
+	}
+	
+	
+	
+
 	@ResponseBody
 	@RequestMapping(value = "/Logout")
 	String Logout(HttpServletRequest request, HttpServletResponse response) {
@@ -171,5 +263,31 @@ public class UserController {
 		}
 
 		return responseText;
+	}
+
+	private int GetCurrentUserId(HttpServletRequest request) {
+
+		int userId = 0;
+
+		Cookie[] cookies = request.getCookies();
+		if (cookies != null) {
+			for (int i = 0; i < cookies.length; i++) {
+				String cookieName = cookies[i].getName();
+				if (cookieName.equals("loginCookie")) {
+					String cookieValue = "";
+
+					try {
+						cookieValue = java.net.URLDecoder.decode(cookies[i].getValue(), "UTF-8");
+					} catch (UnsupportedEncodingException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					String[] parts = cookieValue.split("_");
+					userId = Integer.parseInt(parts[0]);
+				}
+			}
+		}
+
+		return userId;
 	}
 }
